1657b9415501639cc5eedd895d1fb8b0c2412a04
tavstal <tavstal@gmail.com>
2025-07-10 17:47:44 +0200

    Update InstallCompleteController and InstallCompleteView for localization and loading state adjustments

--- src/main/java/io/github/tavstal/mmcinstaller/controllers/InstallCompleteController.java
+++ src/main/java/io/github/tavstal/mmcinstaller/controllers/InstallCompleteController.java
@@ -2,8 +2,7 @@ package io.github.tavstal.mmcinstaller.controllers;
 
 import io.github.tavstal.mmcinstaller.InstallerApplication;
 import io.github.tavstal.mmcinstaller.core.InstallerLogger;
-import io.github.tavstal.mmcinstaller.core.Translator;
-import javafx.event.ActionEvent;
+import io.github.tavstal.mmcinstaller.core.InstallerTranslator;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -14,33 +13,50 @@ import javafx.scene.text.Text;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * Controller class for the installation completion screen.
+ * Implements the `Initializable` interface to initialize UI components and handle events.
+ */
 public class InstallCompleteController implements Initializable {
     private InstallerLogger _logger; // Logger instance for logging events.
-    public Button finishButton;
-    public Label finishedTitle;
-    public Text finishedDescription;
-    public Text finishedAction;
-    public CheckBox launchGameCheckBox;
+    public Button finishButton; // Button to finish the installation process.
+    public Label finishedTitle; // Label displaying the title of the completion screen.
+    public Text finishedDescription; // Text displaying the description of the completion screen.
+    public Text finishedAction; // Text displaying the action message on the completion screen.
+    public CheckBox launchGameCheckBox; // CheckBox to allow the user to choose whether to launch the game.
 
+    /**
+     * Initializes the controller after its root element has been completely processed.
+     * Sets localized text for UI components using the `Translator` instance.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or null if the location is not known.
+     * @param resources The resources used to localize the root object, or null if the root object was not localized.
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         _logger = InstallerApplication.getCustomLogger().WithModule(this.getClass());
-        // Translator instance for localization.
-        Translator _translator = InstallerApplication.getTranslator();
+        InstallerTranslator _translator = InstallerApplication.getTranslator();
 
+        // Set localized text for UI elements.
+        finishedTitle.setText(_translator.Localize("Complete.Title"));
+        finishedDescription.setText(_translator.Localize("Complete.Description"));
+        finishedAction.setText(_translator.Localize("Complete.Action"));
+        launchGameCheckBox.setText(_translator.Localize("Complete.LaunchGameCheckBox"));
         finishButton.setText(_translator.Localize("Common.Finish"));
-
-        // TODO:
-        //  Localize the labels and texts
-        //  Auto launch game checkbox
     }
 
+    /**
+     * Handles the action when the finish button is clicked.
+     * If the "Launch Game" checkbox is selected, the game will be launched (to be implemented).
+     * Exits the application after handling the action.
+     */
     @FXML
     protected void onFinishButtonClick() {
+        if (launchGameCheckBox.isSelected()) {
+            _logger.Debug("Launch game checkbox is selected, launching the game...");
+            // TODO:
+            //  Auto launch game checkbox
+        }
         System.exit(0);
-    }
-
-    public void onLaunchGameCheckBoxChange(ActionEvent actionEvent) {
-
     }
 }
