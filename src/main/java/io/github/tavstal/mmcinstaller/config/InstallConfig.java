ccae424f76e06e09d52e996bb56e1a8a33601030
tavstal <tavstal@gmail.com>
2025-07-11 16:08:40 +0200

    Add macOS application configuration support

--- src/main/java/io/github/tavstal/mmcinstaller/config/InstallConfig.java
+++ src/main/java/io/github/tavstal/mmcinstaller/config/InstallConfig.java
@@ -4,7 +4,7 @@ package io.github.tavstal.mmcinstaller.config;
  * Represents the configuration for the installation process.
  * <br/>
  * This record is used to store various script and executable configurations required during installation,
- * as well as default directory settings.
+ * as well as default directory settings and macOS application configuration.
  *
  * @param defaultDirs The configuration for default directories used during installation.
  * @param batch The configuration for the batch script.
@@ -12,6 +12,7 @@ package io.github.tavstal.mmcinstaller.config;
  * @param zsh The configuration for the zsh script.
  * @param exe The configuration for the executable file.
  * @param linuxDesktop The configuration for the Linux desktop entry script.
+ * @param macApp The configuration for the macOS application.
  */
-public record InstallConfig(DefaultDirsConfig defaultDirs, ScriptConfig batch, ScriptConfig bash, ScriptConfig zsh, ExeConfig exe, ScriptConfig linuxDesktop) {
+public record InstallConfig(DefaultDirsConfig defaultDirs, ScriptConfig batch, ScriptConfig bash, ScriptConfig zsh, ExeConfig exe, ScriptConfig linuxDesktop, MacAppConfig macApp) {
 }
---
+++ src/main/java/io/github/tavstal/mmcinstaller/config/MacAppConfig.java
@@ -0,0 +1,14 @@
+package io.github.tavstal.mmcinstaller.config;
+
+/**
+ * Represents the configuration for a macOS application.
+ * <br/>
+ * This record is used to store the file name, Info.plist content, and script
+ * associated with the macOS application.
+ *
+ * @param fileName The name of the macOS application file.
+ * @param infoList The content of the Info.plist file for the macOS application.
+ * @param script The script associated with the macOS application.
+ */
+public record MacAppConfig(String fileName, String infoList, String script) {
+}
