acd4945cff843ca2a70ae8b9539e15ed6d02966f
tavstal <tavstal@gmail.com>
2025-07-15 00:41:16 +0200

    Add constants for window dimensions and OS name retrieval in Constants class

--- src/main/java/io/github/tavstal/mmcinstaller/core/Constants.java
+++ src/main/java/io/github/tavstal/mmcinstaller/core/Constants.java
@@ -1,10 +1,21 @@
 package io.github.tavstal.mmcinstaller.core;
 
+/**
+ * A utility class that defines constants used throughout the application.
+ */
 public class Constants {
+    /** The name of the operating system, retrieved from system properties and converted to lowercase. */
     public final static String OS_NAME = System.getProperty("os.name").toLowerCase();
 
+    /** The width of the language selection window in pixels. */
     public final static int LANG_WIDTH = 400;
+
+    /** The height of the language selection window in pixels. */
     public final static int LANG_HEIGHT = 200;
+
+    /** The default width of the main application window in pixels. */
     public final static int WIDTH = 700;
+
+    /** The default height of the main application window in pixels. */
     public final static int HEIGHT = 400;
 }
